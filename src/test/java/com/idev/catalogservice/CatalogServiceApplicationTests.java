c5b8a92c495c4958da7770ee0dea07e8dcaa001e
Idodoo <dodoo.ivan@yahoo.com>
2025-06-18 17:35:04 +0000

    build-pipeline10

--- src/test/java/com/idev/catalogservice/CatalogServiceApplicationTests.java
+++ src/test/java/com/idev/catalogservice/CatalogServiceApplicationTests.java
@@ -2,9 +2,11 @@ package com.idev.catalogservice;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 
 
 @SpringBootTest
+@ActiveProfiles("test")
 class CatalogServiceApplicationTests {
 
     @Test
